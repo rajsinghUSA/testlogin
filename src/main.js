a6dc635ef5f9367c2d837838b19e233204c7038f
Raj <rajsinghusa@yahoo.com>
2020-03-06 03:41:44 -0500

    initial big commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import Axios from "./plugins/axios";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+Vue.prototype.$http = Axios;
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");

a183f58ca66252903ef93bdc3e1682bdd49a898c
Raj <rajsinghusa@yahoo.com>
2020-04-05 18:30:44 -0400

    still trying to get ACL to work

--- src/main.js
+++ src/main.js
@@ -6,11 +6,12 @@ import store from "./store";
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
 import Acl from "vue-browser-acl";
-import { ACLRules } from "./utils/acl";
+// import { ACLRules } from "./utils/acl";
 
-Vue.use(Acl, store.state.currentUser, ACLRules, {
-  router,
-  failRoute: { path: "/" /*, replace: true*/ }
+const user = { name: "Raj", role: "normal" };
+
+Vue.use(Acl, user, acl => {
+  acl.rule("normal", "pages", user => user.role == "normal");
 });
 
 Vue.use(Buefy, {
